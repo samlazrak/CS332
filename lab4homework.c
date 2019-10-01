61f22e6494dfa26b3f47ca93d66c132b8e4ee414
samlazrak <Lazrak13@uab.edu>
2019-10-01 14:53:38 -0500

    progress2

--- lab4homework.c
+++ lab4homework.c
@@ -8,16 +8,29 @@ int main(int argc, char** argv) {
   FILE *data;
   char buffer[255] = {0};
   data = fopen (argv[1], "r");
+  int i = 0;
 
   if (data == NULL){
     printf("No data");
   }
   else {
     while((fgets(buffer, 255, data)) != NULL){
-      puts(buffer);
+      //printf("%s\n", buffer);
+      //printf("%s", buffer);
+      putchar (buffer[7]);
+      //printf("\n");
+      /* for(int i = 0; i<sizeof(buffer)/sizeof(int); i++){
+        array[i] = buffer;
+      } */
     }
     fclose(data);
   }
+  //printf("%d", array[0]);
+
+  /* printf("Printing the array now");
+  for(int i = 0; i<sizeof(buffer)/sizeof(int); i++){
+    printf("%5d\n", buffer[i]);
+  } */
   /* if(write(file, "hello\n", 36) != 36){
 
   } */
