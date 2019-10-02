d8d90bd5d05ab46e7095dffdc7aa9c8f62cd21e0
Samuel A Lazrak <samlazrak@uab.edu>
2019-10-02 10:49:40 -0500

    progress3

--- lab4homework.c
+++ lab4homework.c
@@ -4,21 +4,33 @@
 #include <unistd.h>
 #include <string.h>
 
+#define SIZE 100000
+#define COLS 1
+
 int main(int argc, char** argv) { 
   
+  int i;
   FILE *data;
   char buffer[255] = {0};
   data = fopen (argv[1], "r");
-  int i = 0;
+  char index[SIZE][COLS];
 
   if (data == NULL){
     printf("No data");
   }
   else {
     while((fgets(buffer, 255, data)) != NULL){
-      puts (buffer);  
+      i++;
+      char* token = strtok(buffer, ",");
+      while (token != NULL) { 
+        //printf("%s\n", token);
+        strcpy(index[i], token);
+        printf("\nArray Shit:\n%s\n%d", index[i], i);
+        token = strtok(NULL, ",");
+      }
     }
     fclose(data);
+    printf("\n%s", index[1]);
   }
   return 0;
 }
