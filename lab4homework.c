7ea6a758702f5e92c204413ceb805490cf9d7d38
Samuel A Lazrak <samlazrak@uab.edu>
2019-10-01 19:04:59 -0500

    reset

--- lab4homework.c
+++ lab4homework.c
@@ -2,9 +2,10 @@
 #include <string.h> 
 #include <fcntl.h>
 #include <unistd.h>
+#include <string.h>
 
 int main(int argc, char** argv) { 
-  //int file = open("NameList.csv", O_RDONLY);
+  
   FILE *data;
   char buffer[255] = {0};
   data = fopen (argv[1], "r");
@@ -15,25 +16,9 @@ int main(int argc, char** argv) {
   }
   else {
     while((fgets(buffer, 255, data)) != NULL){
-      //printf("%s\n", buffer);
-      //printf("%s", buffer);
-      putchar (buffer[7]);
-      //printf("\n");
-      /* for(int i = 0; i<sizeof(buffer)/sizeof(int); i++){
-        array[i] = buffer;
-      } */
+      puts (buffer);  
     }
     fclose(data);
   }
-  //printf("%d", array[0]);
-
-  /* printf("Printing the array now");
-  for(int i = 0; i<sizeof(buffer)/sizeof(int); i++){
-    printf("%5d\n", buffer[i]);
-  } */
-  /* if(write(file, "hello\n", 36) != 36){
-
-  } */
-
   return 0;
 }
