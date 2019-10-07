e53856d74c4ef638aa0622b74cc268805f6b159d
Sam Lazrak <Lazrak13@uab.edu>
2019-10-07 15:08:24 -0500

    project1

--- lab4homework.c
+++ lab4homework.c
@@ -5,7 +5,7 @@
 #include <string.h>
 
 #define SIZE 100000
-#define COLS 1
+#define COLUMNS 1
 
 int main(int argc, char** argv) { 
   
@@ -13,7 +13,7 @@ int main(int argc, char** argv) {
   FILE *data;
   char buffer[255] = {0};
   data = fopen (argv[1], "r");
-  char index[SIZE][COLS];
+  char index[SIZE][COLUMNS];
 
   if (data == NULL){
     printf("No data");
@@ -23,13 +23,15 @@ int main(int argc, char** argv) {
       i++;
       char* token = strtok(buffer, ",");
       while (token != NULL) { 
-        //printf("%s\n", token);
         strcpy(index[i], token);
-        printf("\nArray Shit:\n%s\n%d", index[i], i);
+        printf("\nArray:\n%s\n%d", index[i], i);
         token = strtok(NULL, ",");
       }
     }
     fclose(data);
+    // ./lab4homework NameList.csv
+    // I don't understand why this prints what it does
+    // I don't understand how to put the seperated strings into an array to then sort them
     printf("\n%s", index[1]);
   }
   return 0;
