693733f2091503830b6fe5a72637d4e320986b1f
Sam Lazrak <Lazrak13@uab.edu>
2019-10-09 16:31:21 -0500

    everything except specific folder and options works

--- Project1/project1.c
+++ Project1/project1.c
@@ -4,6 +4,8 @@ BlazerId: Lazrak13
 Project #: 1
 */
 
+// Everything except specific folder and options works
+
 #include <unistd.h>
 #include <sys/types.h>
 #include <dirent.h>
@@ -15,16 +17,20 @@ void listdir(const char *name, int indent, const char *options, const char *args
   DIR *dir;
   struct dirent *entry;
 
+  //Root Directory files
+
   if (!(dir = opendir(name)))
       return;
-
+  
   while ((entry = readdir(dir)) != NULL) {
     if (entry->d_type != DT_DIR) {
-      if (strcmp(options, "f") == 0){
-
+      if (strcmp(options, "f") == 0) {
+        if(strstr(entry->d_name, args)) {
+          printf("%s\n", entry->d_name);
+        }
       } 
       else if (strcmp(options, "s") == 0) {
-        if (entry->d_reclen >= atoi(args)){
+        if (entry->d_reclen >= atoi(args)) {
           printf("%*s- %s\n", indent, "", entry->d_name);
         }
       } 
@@ -34,65 +40,91 @@ void listdir(const char *name, int indent, const char *options, const char *args
     }
   }
 
+  // Recreating for folders
   if (!(dir = opendir(name)))
       return;
 
+  //Recursive folder and files within them
   while ((entry = readdir(dir)) != NULL) {
     char path[1024];
+    // Omits . and .. entries
     if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
             continue;
+    // Sets path for recursion
     snprintf(path, sizeof(path), "%s/%s", name, entry->d_name);
+    // If entry is a dir
     if (entry->d_type == DT_DIR) {
-      if (strcmp(options, "f") == 0){
-
+      // if f option
+      if (strcmp(options, "f") == 0) {
+        // if name of entry contains -f args
+        if(strstr(entry->d_name, args)) {
+          printf("%s\n", entry->d_name);
+        }
       } 
+      // if s option
       else if (strcmp(options, "s") == 0) {
-        if (entry->d_reclen >= atoi(args)){
+        // if entry has size greater than or equal to -s arg
+        if (entry->d_reclen >= atoi(args)) {
           printf("%*s- %s\n", indent, "", entry->d_name);
         }
       } 
+      // else if no options
       else {
         printf("%*s[%s]\n", indent -4, "", entry->d_name);
       }
-    } 
+    }
+    // if entry is not a directory 
     else if (entry->d_type != DT_DIR) {
+      // checks if entry is in the root folder and not a sub folder
       if(entry->d_namlen+2 != strlen(path)) {
-        if (strcmp(options, "f") == 0){
-
+        // if -f option
+        if (strcmp(options, "f") == 0) {
+          // checks if checks if entry containts -f args
+          if(strstr(entry->d_name, args)) {
+            printf("%s\n", entry->d_name);
+          }
         } 
+        // if -s option
         else if (strcmp(options, "s") == 0) {
-          if (entry->d_reclen >= atoi(args)){
+          // checks if entry has size greater than or equal to -s arg
+          if (entry->d_reclen >= atoi(args)) {
             printf("%*s- %s\n", indent, "", entry->d_name);
           }
         } 
+        // if no options
         else {
             printf("%*s- %s\n", indent, "", entry->d_name);
         }
       }
     }
+    // recursive call
     listdir(path, indent, options, args);
   }
   closedir(dir);
 }
 
 int main(int argc, char** argv) {
+  // If no arguments do current folder with no options or args
   if (argc == 1) {
     listdir(".", 4, ".", ".");
     return 0;
   }
+
+  // checks for -s or -f options
   int opt;
   while (optind < argc) {
       if ((opt = getopt(argc, argv, "f:s:")) != -1) {
         switch(opt) {
           case 'f':
-            if(argc == 3) {
+            if(strcmp(argv[1], ".") == 0) {
               listdir(argv[1], 4, "f", optarg);
             }
             else {
               listdir(".", 4, "f", optarg);
             }
+            break;
           case 's':
-            if(argc == 5) {
+            if(strcmp(argv[1], ".") == 0) {
               listdir(argv[1], 4, "s", optarg);
             }
             else {
@@ -103,11 +135,8 @@ int main(int argc, char** argv) {
       }
       else {
         optind++;
-        if(argc == 2) {
-          listdir(argv[1], 4, ".", ".");
-        } else {
-          printf("ERROR\n");
-        }
+        // allows for specific folder call
+        listdir(argv[1], 4, ".", ".");
       }
     return 0;
   }
